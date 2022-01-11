b39259210eaa7f503c2a8f9d5fe4caac44706e6a
Matthew Lemon <chaffinach+git@protonmail.ch>
2022-01-11 17:24:52 +0000

    added a script to be called by newsboat to journal urls

---
+++ get-newsboat-comment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 01/11/2022
+# Get the user to type something
+# for use in newsboat config.
+# This is triggered by browser macro hack (,y) - you type a comment
+# then this script pipes it to _tj
+echo "Enter a comment: "
+read -r comment
+echo -e "$comment": "$1"|_tj # I can't be bothered checking whether `_tj` exists...
+
