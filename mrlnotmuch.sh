2191e2ad9d6086dfb403b480c8ab3ffdfc072d8c
Matthew Lemon <matt@matthewlemon.com>
2023-01-13 08:26:58 +0000

    tweak

--- mrlnotmuch.sh
+++ mrlnotmuch.sh
@@ -5,7 +5,7 @@ notmuch tag +emacs from:info@protesilaos.com > /dev/null 2>&1
 notmuch tag +list +emacs to:~protesilaos/denote@lists.sr.ht > /dev/null 2>&1
 notmuch tag +list from:ek@eleanorkonik.com > /dev/null 2>&1
 notmuch tag +receipt from:platform@parentpay.com > /dev/null 2>&1
-notmuch tag +todo to:madmin+task@rushpost.com > /dev/null 2>&1
+notmuch tag +todo +unread from:matt@matthewlemon.com to:madmin+task@rushpost.com > /dev/null 2>&1
 notmuch tag +school from:SC9294404a@schoolcomms.com > /dev/null 2>&1
 notmuch tag +school from:27ED003@groupcallalert.com > /dev/null 2>&1
 notmuch tag +deleted from:verify@twitter.com > /dev/null 2>&1
