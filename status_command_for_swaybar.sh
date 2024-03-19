c8eb3067fb0886723a2565e33776cadc6e90f2ec
Matthew Lemon <y@yulqen.org>
2024-03-19 08:23:37 +0000

    Adds battery info to sway statusbar

--- status_command_for_swaybar.sh
+++ status_command_for_swaybar.sh
@@ -1,8 +1,8 @@
 #/bin/sh
  
 net="$(ip a|grep "inet 192"|cut -d' ' -f6|cut -d'/' -f1)"
-toss="Toss!"
 packages="$(dpkg --get-selections|wc -l)"
 weather="$(ansiweather -l Berwick-upon-Tweed,GB -f 1 -a false -s true -w true -d true)"
+batt="$(cat /sys/class/power_supply/BAT0/capacity)"
 
-echo "$weather | dpkg: $packages | $net | $(date +"%Y-%m-%d %I:%M:%S %p")"
+echo "$weather | dpkg: $packages | bat: $batt | $net | $(date +"%Y-%m-%d %I:%M:%S %p")"
