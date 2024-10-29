30bb53c4b8a4eb7747e12bc930161892127fc93a
Matthew Lemon <y@yulqen.org>
2024-10-29 12:10:38 +0000

    Better command to launch openbsd vm with graphical interface

--- launch_openbsd_vm_with_display.sh
+++ launch_openbsd_vm_with_display.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-qemu-system-x86_64 -cpu host -accel kvm -m 4096 -drive file=$HOME/vms/os/openbsd7.6/openbsd.qcow2 -display default
+qemu-system-x86_64 -cpu host -accel kvm -m 4096 -drive file=$HOME/vms/os/openbsd7.6/openbsd.qcow2 -display default -nic user,hostfwd=tcp::10022-:22 -vga std
